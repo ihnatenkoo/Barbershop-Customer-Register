892f39b27c0a669f76173fbb084c9acfad79f83a
Dmytro <ihnatenko.dev@gmail.com>
2023-03-24 12:13:15 +0200

    Create mock data with masters and services info

---
+++ src/mock-data/masters.ts
@@ -0,0 +1,12 @@
+export const masters = [
+	{
+		_id: '630cd062651379283779ccfa',
+		name: 'Raffaele Vigliotta',
+		avatar: 'https://i.ibb.co/vZrYQc1/image.png',
+	},
+	{
+		_id: '630cd098651379283779ccfc',
+		name: 'Vincenzo Cioffi',
+		avatar: 'https://i.ibb.co/jyqmnNj/image.png',
+	},
+];
