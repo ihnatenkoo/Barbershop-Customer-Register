76db581b40f240a4a28340fbf318d000d10c512f
Dmytro <ihnatenko.dev@gmail.com>
2023-04-27 15:04:44 +0300

    Update <Calendar/>

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,11 +1,18 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import ReactCalendar from 'react-calendar';
-import { useAppDispatch } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import { addOrderDay } from '../../store/order/order.slice';
+import { selectUnformattedCurrentDate } from '../../store/order/selectors';
+import { calculateInitialDate } from '../../utils/calculateInitialDate';
 import './calendar.css';
 
 const Calendar: FC = () => {
 	const dispatch = useAppDispatch();
+	const orderDay = useAppSelector(selectUnformattedCurrentDate);
+
+	useEffect(() => {
+		dispatch(addOrderDay(calculateInitialDate()));
+	}, []);
 
 	const onDateChange = (pickedDate: Date): void => {
 		dispatch(addOrderDay(pickedDate));
@@ -14,8 +21,9 @@ const Calendar: FC = () => {
 	return (
 		<ReactCalendar
 			onChange={onDateChange}
-			value={new Date()}
+			value={orderDay}
 			locale="it"
+			minDate={calculateInitialDate()}
 			minDetail="month"
 			tileDisabled={({ activeStartDate, date, view }) =>
 				date.getDay() == 6 || date.getDay() == 0
